4d416a1e7ab626eb8845d1e98a200c0a88c59c5d
Shray Arora <shrayarora@Shrays-MacBook-Pro.local>
2020-05-30 16:57:03 -0700

    main of progress added

---
+++ progress/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	addr := os.Getenv("ADDR")
+	if len(addr) == 0 {
+		addr = ":80"
+	}
+
+	dsn := os.Getenv("DSN")
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		fmt.Printf("error opening database: %v\n", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	ctx := NewMySQLStore(db)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/v1/progress", ctx.ProgressUserHandler)
+
+	log.Printf("server is listening at %s...", addr)
+	log.Fatal(http.ListenAndServe(addr, r))
+}
